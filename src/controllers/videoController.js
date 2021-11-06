8226dcb3c2e1f4bc99122c9f26df7f53cce94284
suhapy <70417878+suhapy@users.noreply.github.com>
2021-11-06 19:17:04 +0900

    Update videoController,js

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
-  return res.render("home", { pageTitle: "Home:", videos });
+  return res.render("home", { pageTitle: "Home", videos });
 };
 export const see = (req, res) => {
   // const id = req.params.id;
@@ -18,18 +18,20 @@ export const postEdit = (req, res) => {
   const { title } = req.body;
   return res.redirect(`/videos/${id}`);
 };
-export const getUpload = (req, res) => {
-  return res.render("upload", { pageTitle: "Upload Video" });
-};
-export const postUpload = (req, res) => {
+export const getUpload = (req, res) => {};
+export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  const video = new Video({
-    title,
-    description,
-    createdAt: Date.now(),
-    hashtags: hashtags.split(",").map((word) => `#${word}`),
-    meta: { views: 0, rating: 0 },
-  });
-  console.log(video);
-  return res.redirect("/");
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
